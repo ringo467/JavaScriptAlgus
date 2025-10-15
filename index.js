116b14771b862dcff428a32a57e4fc19602ae0a7
unknown <ringo.fedotov@voco.ee>
2025-10-15 15:10:14 +0300

    tunnitoo

---
+++ index.js
@@ -0,0 +1,26 @@
+//console.log('Hei');
+//console.log('pitsa');
+
+//window.alert('See on alarm!!!');
+//window.alert('See o´pitsa!');
+document.getElementById('myH1').textContent='Hei'
+document.getElementById('myP').textContent='juust'
+// kommentaar
+
+/* kommen-
+taar*/
+
+//variables aka muutujad
+
+let x;
+x = 100;
+
+console.log(x);
+
+let age = 25;
+let price = 2.50;
+console.log(age);
+console.log(price);
+
+console.log(`You are ${age} years old`);
+console.log(`Hind on $${price}`);
