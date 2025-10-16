d5fb3f08211153f7eee1a3e5faf56e32881105b3
unknown <ringo.fedotov@voco.ee>
2025-10-16 10:03:59 +0300

    koolitoo

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 
 //window.alert('See on alarm!!!');
 //window.alert('See o´pitsa!');
-document.getElementById('myH1').textContent='Hei'
-document.getElementById('myP').textContent='juust'
+//document.getElementById('myH1').textContent='Hei'
+//document.getElementById('myP').textContent='juust'
 // kommentaar
 
 /* kommen-
@@ -12,15 +12,80 @@ taar*/
 
 //variables aka muutujad
 
-let x;
-x = 100;
+//let x;
+//x = 100;
 
-console.log(x);
+//console.log(x);
 
-let age = 25;
-let price = 2.50;
-console.log(age);
-console.log(price);
+//let age = 25;
+//let price = 2.50;
+//console.log(age);
+//console.log(price);
 
-console.log(`You are ${age} years old`);
-console.log(`Hind on $${price}`);
+//console.log(`You are ${age} years old`);
+//console.log(`Hind on $${price}`);
+
+// A on muutuja, mis hoiab valuet, ta on nagu it cont
+//let fullName="Ringo Fedotov";
+//let age=26;
+//let isStudent=true;
+//let lause = "Tere" + " "+ fullName + " "+ age+".";
+
+//document.getElementById("p1").textContent= `Mu nimi on ${fullName}`;
+//document.getElementById("p2").textContent=`Mu vanus on ${age}`;
+//document.getElementById("p3").textContent=`Ma olen õpilane? ${isStudent}`;
+//console.log(document);
+//console.log(window);
+//console.log(lause)
+
+//aritmeetilised muutujad
+
+//let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students **2; see on ruudus ehk astmes 2
+//students = students **3; see on kuubis
+//students = students % 2; leiab jäägi
+//students = students % 4;
+//let extraStudents = students % 3;
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students= **= 2;
+//students %=2;
+
+//students++;
+//students--;
+
+//console.log(students);
+
+
+//let result = 1 + 2 * 3 + 4 ** 2;
+/* arvuti teeb alguses sulgudes, siis astmed, siis korrutamine/jagamaine ja siis liitmine/lahutamine*/
+
+//console.log(result)
+
+//accept user input
+// kerge on window prompt
+//raske on html textbox
+
+//LIHTNE
+
+//let username;
+
+//username = window.prompt("Mis su kasutajanimi on?");
+
+//console.log(username);
+
+//RASKE (indexis on ka bodys nupp jne tehtud)
+
+let username;
+
+document.getElementById("mySubmit").onclick = function (){
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
