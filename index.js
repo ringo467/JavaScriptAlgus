7fd8310db1b81a7eaba91f07bce6d4f6d9c5bbee
unknown <ringo.fedotov@voco.ee>
2025-10-16 10:54:23 +0300

    tunnitoo

--- index.js
+++ index.js
@@ -114,20 +114,21 @@ taar*/
 
 // const= variable, mida ei saa muuta peale sisestamist
 
-const PI = 3.14159
-let radius;
-let circumference;
+//const PI = 3.14159
+//let radius;
+//let circumference;
 
 //radius = window.prompt(`Siesta ringi raadius`);
 //radius = Number(radius);
 
 
 
-document.getElementById("mySubmit").onclick = function (){
-radius = document.getElementById("myText").value;
-radius = Number(radius);
-circumference = 2 * PI * radius;
-document.getElementById("myH3").textContent = circumference +"cm";
-}
+//document.getElementById("mySubmit").onclick = function (){
+//radius = document.getElementById("myText").value;
+//radius = Number(radius);
+//circumference = 2 * PI * radius;
+//document.getElementById("myH3").textContent = circumference +"cm";
+//}
 
-console.log(circumference)
+//console.log(circumference)
+
