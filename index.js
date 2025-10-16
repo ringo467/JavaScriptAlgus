8a86a24dffef1bdaf1157017581786821d83a280
unknown <ringo.fedotov@voco.ee>
2025-10-16 11:25:56 +0300

    programm1

--- index.js
+++ index.js
@@ -1,133 +1,20 @@
-//console.log('Hei');
-//console.log('pitsa');
+//PROGRAMM
 
-//window.alert('See on alarm!!!');
-//window.alert('See o´pitsa!');
-//document.getElementById('myH1').textContent='Hei'
-//document.getElementById('myP').textContent='juust'
-// kommentaar
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
 
-/* kommen-
-taar*/
-
-//variables aka muutujad
-
-//let x;
-//x = 100;
-
-//console.log(x);
-
-//let age = 25;
-//let price = 2.50;
-//console.log(age);
-//console.log(price);
-
-//console.log(`You are ${age} years old`);
-//console.log(`Hind on $${price}`);
-
-// A on muutuja, mis hoiab valuet, ta on nagu it cont
-//let fullName="Ringo Fedotov";
-//let age=26;
-//let isStudent=true;
-//let lause = "Tere" + " "+ fullName + " "+ age+".";
-
-//document.getElementById("p1").textContent= `Mu nimi on ${fullName}`;
-//document.getElementById("p2").textContent=`Mu vanus on ${age}`;
-//document.getElementById("p3").textContent=`Ma olen õpilane? ${isStudent}`;
-//console.log(document);
-//console.log(window);
-//console.log(lause)
-
-//aritmeetilised muutujad
-
-//let students = 30;
-
-//students = students + 1;
-//students = students - 1;
-//students = students * 2;
-//students = students / 2;
-//students = students **2; see on ruudus ehk astmes 2
-//students = students **3; see on kuubis
-//students = students % 2; leiab jäägi
-//students = students % 4;
-//let extraStudents = students % 3;
-//students += 1;
-//students -= 1;
-//students *= 2;
-//students /= 2;
-//students= **= 2;
-//students %=2;
-
-//students++;
-//students--;
-
-//console.log(students);
-
-
-//let result = 1 + 2 * 3 + 4 ** 2;
-/* arvuti teeb alguses sulgudes, siis astmed, siis korrutamine/jagamaine ja siis liitmine/lahutamine*/
-
-//console.log(result)
-
-//accept user input
-// kerge on window prompt
-//raske on html textbox
-
-//LIHTNE
-
-//let username;
-
-//username = window.prompt("Mis su kasutajanimi on?");
-
-//console.log(username);
-
-//RASKE (indexis on ka bodys nupp jne tehtud)
-
-//let username;
-
-//document.getElementById("mySubmit").onclick = function (){
-  //  username = document.getElementById("myText").value;
-   // document.getElementById("myH1").textContent = `Heihopsti ${username}`
-  //  console.log(username);
-//}
-
-// TÜÜBI MUUTMINE
-
-//let age = window.prompt("Vana sa oled?");
-//age = Number(age);
-//age+=1;
-
-//console.log(age, typeof age);
-
-//let x = "pitsa";
-//let y = "pitsa";
-//let z = "pitsa";
-
-//x = Number(x);
-//y = String(y);
-//z = Boolean(z)
-
-//console.log(x, typeof x);
-//console.log(y, typeof y);
-//console.log(z, typeof z);
-
-
-// const= variable, mida ei saa muuta peale sisestamist
-
-const PI = 3.14159
-let radius;
-let circumference;
-
-//radius = window.prompt(`Siesta ringi raadius`);
-//radius = Number(radius);
-
-
-
-document.getElementById("mySubmit").onclick = function (){
-radius = document.getElementById("myText").value;
-radius = Number(radius);
-circumference = 2 * PI * radius;
-document.getElementById("myH3").textContent = circumference +"cm";
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
 }
-
-console.log(circumference)
+decreaseBtn.onclick = function(){
+    count--;
+   countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
