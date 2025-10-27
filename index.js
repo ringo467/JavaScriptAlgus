22c4ee10aba8a5d25b2ed9106351981c6c258ee3
ringo467 <ringo.fedotov@voco.ee>
2025-10-27 14:35:57 +0200

    prog 2

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-//PROGRAMM
+//PROGRAMM 2
 
+var a = 10;
+var b = 20;
+console.log("enne vahetamist: a =", a, ", b =", b);
+var temp = a;
+a = b;
+b = temp;
+
+console.log("peale vahetamist: a =", a, ", b =", b);
