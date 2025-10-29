6574e539d9692116b31aac24000b8fad92fc08b7
ringo467 <ringo.fedotov@voco.ee>
2025-10-29 13:10:50 +0200

    vanuse ül

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-//PROGRAMM 2
+//PROGRAMM 3
 
-var a = 10;
-var b = 20;
-console.log("enne vahetamist: a =", a, ", b =", b);
-var temp = a;
-a = b;
-b = temp;
+function kontrolliVanust() {
+    var sisend = prompt("Siseta oma vanus");
+    if (sisend === "" || isNaN(sisend)) {
+        alert ("Sisend pole korrektne, palun sisesta number");
+        kontrolliVanust();
+        return
+    }
 
-console.log("peale vahetamist: a =", a, ", b =", b);
+    var vanus = Number(sisend);
+    if (vanus >= 7 && vanus <= 16) {
+        alert("Oled p]hikooli opilane ja sood kell 10");
+    } else if (vanus >= 17 && vanus <=20) {
+        alert("Oled gumnaasiumi opilane ja sood kell 11");
+    } else if (vanus > 20) {
+        alert ("Oled taiskasvanu ja sood kell 12");
+    } else {
+        alert ("Oled liiga noor, et koolis olla");
+    }
+}
+
+kontrolliVanust();
